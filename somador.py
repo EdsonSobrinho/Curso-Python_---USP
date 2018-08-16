f0719f4c9ecbdf355edddd443f2f61ac583f23bf
EdsonSobrinho <edsonpaulosobrinho@gmail.com>
2018-08-16 14:52:12 -0300

    git add *.*
    ok

---
+++ somador.py
@@ -0,0 +1,35 @@
+a = 10
+b = 20
+
+soma = a +b
+
+print("A soma dos números é:", soma)
+
+x = 10
+y = 30
+x = x + 10
+y = x + 10
+
+print (x)
+
+x = x + y
+
+print ("A soma de x = x + y é igual a:", x)
+
+x = 50
+y = 20
+
+aux = x
+
+x=y
+
+y = aux
+
+print (x) 
+
+print (y)
+
+a = 1
+b = 2
+
+print ("a + 2 * b é igual a:", a + 2 * b)
